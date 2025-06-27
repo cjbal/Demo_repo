84882e378ed7362641e0c5162b16a8632f3ad78f
cj <cjsinghus@gmail.com>
2025-06-26 22:15:14 -0400

    last maths rev

--- maths.py
+++ maths.py
@@ -6,5 +6,11 @@ print("Addition:", result1)
 result2 = num1 - num2
 print("Subtraction:", result2)
 
-result3= num1.num2
+result3 = num1*num2
 print("Multiplication:",result3)
+
+result4 = num1/num2
+print("Division:",result4) 
+
+result5 = round(3.456,2)
+print("Round 3.456 to tens place:", result5)
