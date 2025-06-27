852e1a00e29e4c9833dc861ed968a74a671b3af9
cj <cjsinghus@gmail.com>
2025-06-26 22:51:40 -0400

    2nd int rev

--- match.py
+++ match.py
@@ -1,6 +1,6 @@
 import re
 text = "Punjabi shaa gaye oye"
-search = r"Punjabi shaa"
+search = r"Punjabi shaa gaye oye"
 match = re.match(search,text)
 if match:
    print("Match Found:", match.group())
